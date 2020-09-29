5981957b9bc2d8a081e2edc8674e42b3a4abba17
Rohit Saini <sketchlearn7@gmail.com>
2020-09-29 16:50:34 +0530

    Initial Commit

---
+++ Giggle/GiggleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GiggleApp.swift
+//  Giggle
+//
+//  Created by Rohit Saini on 29/09/20.
+//
+
+import SwiftUI
+
+@main
+struct GiggleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
