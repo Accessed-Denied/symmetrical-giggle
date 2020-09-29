846db3262a1f06f84129a8be68e6ad239ae535d6
Rohit Saini <sketchlearn7@gmail.com>
2020-09-29 23:41:57 +0530

    navigation added on button click

--- Giggle/LandingView.swift
+++ Giggle/LandingView.swift
@@ -8,45 +8,52 @@
 import SwiftUI
 
 struct LandingView: View {
+    @State private var isActive = false
     var body: some View {
-        GeometryReader{ proxy in
-            VStack{
-                Spacer().frame(height:proxy.size.height * 0.25)
-                Text("Increment")
-                    .font(.system(size: 64))
-                    .foregroundColor(.white)
-                    .fontWeight(.heavy)
-                Spacer()
-                Button(action: {}){
-                    HStack{
-                        Spacer()
-                        Image(systemName: "plus.circle")
-                            .font(.system(size: 24,weight: .medium))
-                            .foregroundColor(.white)
-                        Text("Create a challenge")
-                            .font(.system(size: 24,weight: .medium))
-                            .foregroundColor(.white)
-                        Spacer()
+        NavigationView{
+            GeometryReader{ proxy in
+                VStack{
+                    Spacer().frame(height:proxy.size.height * 0.25)
+                    Text("Increment")
+                        .font(.system(size: 64))
+                        .foregroundColor(.white)
+                        .fontWeight(.heavy)
+                    Spacer()
+                    NavigationLink(destination: Text("Navigated"),isActive: $isActive){
+                        Button(action: {
+                            isActive = true
+                        }){
+                            HStack{
+                                Spacer()
+                                Image(systemName: "plus.circle")
+                                    .font(.system(size: 24,weight: .medium))
+                                    .foregroundColor(.white)
+                                Text("Create a challenge")
+                                    .font(.system(size: 24,weight: .medium))
+                                    .foregroundColor(.white)
+                                Spacer()
+                            }
+                        }
+                        .padding(.horizontal,16)
+                        .buttonStyle(PrimaryButtonStyle())
                     }
-                }
-                .padding(.horizontal,16)
-                .buttonStyle(PrimaryButtonStyle())
-
-            }.frame(
-                maxWidth: .infinity,
-                maxHeight: .infinity
-            )
-            .background(
-                Image("landingBg")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: proxy.size.width)
-                    .overlay(Color.black.opacity(0.4))
-                    .edgesIgnoringSafeArea(.all)
-            )
-            
+                    
+                    
+                }.frame(
+                    maxWidth: .infinity,
+                    maxHeight: .infinity
+                )
+                .background(
+                    Image("landingBg")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: proxy.size.width)
+                        .overlay(Color.black.opacity(0.4))
+                        .edgesIgnoringSafeArea(.all)
+                )
+                
+            }
         }
-        
     }
 }
 
